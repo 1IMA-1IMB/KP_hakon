448bf66f62b10d2d0070d390cb34a49efc74e025
Mud <148325452+Mudderr@users.noreply.github.com>
2024-01-03 11:29:21 +0100

    javascript intro

---
+++ JSintro/JS/index.js
@@ -0,0 +1,13 @@
+ // Heisann
+ console.log('Hello World');
+
+ // oppgave a 
+ let firstName = 'Håkon';
+ let age ='16 years old';
+ console.log('firstname');
+ console.log('age');
+
+ //oppgave b
+ let name='Hei';
+
+ //  Let kommand brukes til å lage variabler
