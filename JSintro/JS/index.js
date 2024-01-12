efe4523ac4ea59742f4f2b99d7f8d2959e1da853
Mud <148325452+Mudderr@users.noreply.github.com>
2024-01-12 09:18:39 +0100

    manyyy changes
    - more css and html
    - written a lot of content

--- JSintro/JS/index.js
+++ JSintro/JS/index.js
@@ -1,13 +1,54 @@
  // Heisann
  console.log('Hello World');
 
- // oppgave a 
- let firstName = 'Håkon';
- let age ='16 years old';
- console.log('firstname');
- console.log('age');
+ // oppgave A
+    let name = 'Håkon';
+    let age = '16 years old';
+    console.log('name');
+    console.log('age');
 
- //oppgave b
- let name='Hei';
+ //oppgave B
+    var firstName = 'Håkon';
+    var yearsOld = '16 years old';
+    console.log('firstName');
+    console.log('yearsOld');
 
- //  Let kommand brukes til å lage variabler
+ //  var is another command you can use instead of Let, but it is outdated.
+
+ // oppgave C
+    var erEnElev= true;
+    console.log('erEnElev')
+
+// Oppgave D
+
+    if(!erEnElev){
+        console.log('"du er ikke en elev!"');
+    } else {
+        console.log('"du er en elev!"');
+    }
+
+// on the console log it says erEnElev, and underneath "du er en elev!" idk why ngl
+
+// DEL 2
+
+// oppgave A
+
+// Assign values to x and y
+let x = 10;
+let y = 20;
+
+// Add x and y and assign the sum to z
+let z = x + y;
+
+console.log(z);
+
+// oppgave B
+
+function getRandomInt(max){
+    return Math.floor(Math.random()* max);
+}
+console.log(11-(getRandomInt(11)));
+
+
+// oppgave C
+
