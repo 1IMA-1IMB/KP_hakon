154dd59214c8cae9c1be64cc933d4a40d8ddbae0
Mud <148325452+Mudderr@users.noreply.github.com>
2024-05-15 08:28:40 +0200

    new code

---
+++ Database-tilkobling/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var mysql = require('mysql');
+var cors = require('cors')
+
+app.use(cors())
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'hakonkaro_Skole'
+});
+
+connection.connect(function(err) {
+
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+ 
+connection.query('SELECT * FROM elev2', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
+
+// app.get('/', (request, response) => {
+    // if (error) throw error;
+    // response.send(JSON.stringify('Hello World'))
+//})
+
+//app.get('/getallstudents', (request, response) => {
+
+ // connection.query('SELECT * FROM elev', function (error, results, fields) {
+    // if (error) throw error;
+    // response.send(JSON.stringify(results));
+  // }); 
+
+  
+//})
