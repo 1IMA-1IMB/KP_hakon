a511d6b841612f585d17d096e370dd3776f548d6
Mud <148325452+Mudderr@users.noreply.github.com>
2024-05-15 08:39:38 +0200

    new code

--- Database-tilkobling/index.js
+++ Database-tilkobling/index.js
@@ -24,26 +24,29 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
-
- 
 connection.query('SELECT * FROM elev2', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
-});
- 
-connection.end();
-
-// app.get('/', (request, response) => {
-    // if (error) throw error;
-    // response.send(JSON.stringify('Hello World'))
-//})
-
-//app.get('/getallstudents', (request, response) => {
-
- // connection.query('SELECT * FROM elev', function (error, results, fields) {
-    // if (error) throw error;
-    // response.send(JSON.stringify(results));
-  // }); 
+    if (error) throw error;
+    console.log('The solution is: ', results);
+  });
+   
+  connection.end();
 
   
-//})
+app.get('/', (request, response) => {
+    if (error) throw error;
+    response.send(JSON.stringify('Hello World'))
+})
+
+
+app.get('/getallstudents', (request, response) => {
+
+  connection.query('SELECT * FROM elev', function (error, results, fields) {
+    if (error) throw error;
+    response.send(JSON.stringify(results));
+  }); 
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
